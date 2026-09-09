a02edd62976a918c33aebdfecc489abb4239a9d1
Rakotoarilala51 <maitreaina51@gmail.com>
2026-09-09 21:39:01 +0300

    feat(download.ts): add downloadText function for downloading text files

---
+++ lib/download.ts
@@ -0,0 +1,8 @@
+export function downloadText(content: string, filename: string, type: string) {
+  const url = URL.createObjectURL(new Blob([content], { type }));
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = filename;
+  link.click();
+  setTimeout(() => URL.revokeObjectURL(url), 1000);
+}
